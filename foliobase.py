fd1652cd68da395de254913700b0578863c61e25
sev552 <46254861+sev552@users.noreply.github.com>
2020-03-23 17:37:46 -0400

    First Commit!

---
+++ foliobase.py
@@ -0,0 +1,17 @@
+# PyPortfolio - 3/23/2020 - Andrew J Clark
+#
+# Skeleton File of basic single functions, Description of structure and dependencies
+
+# Structure:
+# -
+
+# Basic Dependencies:
+# - Selenium: Webpage manipulation
+# - Beautiful Soup: Parsing HTML
+
+# Aspirational Dependencies:
+# - Pandas: Data Analysis, Modeling
+# - Plotly(?): Data Visualization
+
+#
+# Begin
